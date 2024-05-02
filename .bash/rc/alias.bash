bf2b192d6cf21b5a625bad114bc768116a9291a3
RyoAtWSL_LapTop <drsgon.work.36@gmail.com>
2024-05-03 01:08:09 +0900

    :sparkles: Add alias in bash

---
+++ .bash/rc/alias.bash
@@ -0,0 +1,12 @@
+### ls ###
+alias ls='ls --color=always'
+alias la='ls -AF --color=always'
+alias ll='ls -alF --color=always'
+alias l='ls -CF --color=always'
+
+### grep ###
+alias grep='grep --color=always'
+
+### vim ###
+alias vi='vim.tiny'
+alias view='vim -R'
