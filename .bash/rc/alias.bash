e3ce07ec531eead099a68b5e438c769c51486127
RyoAtWSL_LapTop <drsgon.work.36@gmail.com>
2024-06-15 11:16:45 +0900

    :sparkles: Remove Aliases

--- .bash/rc/alias.bash
+++ .bash/rc/alias.bash
@@ -1,12 +1,10 @@
 ### ls ###
 alias ls='ls --color=always'
-alias la='ls -AF --color=always'
+alias la='ls -aF --color=always'
 alias ll='ls -alF --color=always'
-alias l='ls -CF --color=always'
 
 ### grep ###
 alias grep='grep --color=always'
 
 ### vim ###
-alias vi='vim.tiny'
-alias view='vim -R'
+alias vi='vim'
