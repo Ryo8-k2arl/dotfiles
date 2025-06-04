e2fe11bdbb97480ba2f00e19a220516cf1ae24ee
Ryo@Lap <dragon.work.36@gmail.com>
2025-06-05 01:06:38 +0900

    :zap: Change git-repository's directory

--- .config/zsh/function.zsh
+++ .config/zsh/function.zsh
@@ -1,3 +1,16 @@
 #=================================================================================================#
 ##          New Commands                                      																	 ##
 #=================================================================================================#
+
+FILTER=fzf
+
+# change git-repository's directory
+function FILTER-cd-repo() {
+	local selected_dir=$(ghq list -p | "$FILTER" --query "$LBUFFER")
+	if [ -n "$selected_dir" ]; then
+		BUFFER="cd ${selected_dir}"
+		zle accept-line
+	fi
+	zle clear-screen
+}
+zle -N FILTER-cd-src
