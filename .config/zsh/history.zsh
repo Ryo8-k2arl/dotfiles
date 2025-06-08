138f874ae88d0b00d375d67c93d84230176494c2
Ryo@Lap <dragon.work.36@gmail.com>
2025-06-09 03:54:53 +0900

    :zap: Configure history's value

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -1,3 +1,9 @@
 #=================================================================================================#
 ##					History																																							 ##
 #=================================================================================================#
+
+HISTFILE="${ZDATADIR}/zsh/zsh_history"
+HISTSIZE=10000                    						# Number of histories in memory
+SAVEHIST=100000                   						# Number of histories to be saved
+HISTORY_IGNORE="(l[sal\.]|cd|pwd|zsh|exit|cd ..)"
+LISTMAX=1000                      						# number of completion listings to ask for (1=shut up, 0=ask when window overflows)
