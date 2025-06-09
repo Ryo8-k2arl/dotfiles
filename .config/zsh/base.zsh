89c8da185ce0d372a13599e375ad1e43b449af3e
Ryo@Lap <dragon.work.36@gmail.com>
2025-06-09 11:00:36 +0900

    :sparkles: Define one word when using C-W to delete a word.

--- .config/zsh/base.zsh
+++ .config/zsh/base.zsh
@@ -1,3 +1,8 @@
 #=================================================================================================#
 ##					 Base Configuration																																	 ##
 #=================================================================================================#
+
+# delete word by word with C-w when typing ls /usr/local/etc etc.
+# default : ls /usr/local → ls /usr/ → ls /usr → ls /usr → ls /
+# this setting : ls /usr/local → ls /usr/ → ls /usr/ → ls /
+WORDCHARS='*?_-[]~&;!#$%^(){}<>|'
