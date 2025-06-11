1f04e644bb7a26644cbc37ffa78fc22c15b7e489
Ryo@Lap <dragon.work.36@gmail.com>
2025-06-11 10:24:46 +0900

    :bug: Removing directory duplicates

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -2,7 +2,7 @@
 ##					History																																							 ##
 #=================================================================================================#
 
-HISTFILE="${ZDATADIR}/zsh/zsh_history"
+HISTFILE="${ZDATADIR}/zsh_history"
 HISTSIZE=10000                    						# Number of histories in memory
 SAVEHIST=100000                   						# Number of histories to be saved
 HISTORY_IGNORE="(l[sal\.]|cd|pwd|zsh|exit|cd ..)"
