18151398f19d226d3c7016823295f2c4ba53025a
Ryo @ LAB <dragon.work.36@gmail.com>
2026-06-15 02:25:29 +0900

    :wrench: Add Neovim LSP configs for ruff and ty
    Add new LSP configuration modules:
    - .config/nvim/after/lsp/ruff.lua: register ruff server (`cmd = { "ruff", "server" }`), set `filetypes = { "python" }`, and `init_options.settings.logLevel = "warn"`.
    - .config/nvim/after/lsp/ty.lua: register ty server (`cmd = { "ty", "server" }`) and set `init_options.settings.logLevel = "warn"`.

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -8,6 +8,37 @@ return {
     config = function ()
       require('mason').setup()
     end,
-  }
-}
+    opts = {},
+  },
 
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependecies = { "mason-org/mason.nvim" },
+    cmd = {
+      "MasonToolsInstall",
+      "MasonToolsInstallSync",
+      "MasonToolsUpdate",
+      "MasonToolsUpdateSync",
+    },
+    opts = {
+      ensure_installed = {
+        -- Lua
+        -- Rust
+        "rust-analyzer",
+
+        -- Python
+        "ruff",
+        "ty",
+
+        -- LaTeX
+        "texlab",
+        "latexindent",
+        -- Markdown
+        -- Shell
+        --
+      },
+      auto_update = false,
+      run_on_start = true,
+    },
+  },
+}
