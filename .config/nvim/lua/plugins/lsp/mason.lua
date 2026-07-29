bcb3892d9320486b417ff78fdb6e25ed6888df28
Ryo @ n31's WSL <acdragon.16@mnb.ees.saitama-u.ac.jp>
2026-07-30 02:33:53 +0900

    🎨 Normalize formatting in Mason plugin config
    - Reformat the `mason.nvim` setup block to follow consistent Lua style
    and indentation.
    - Switch quote style to double quotes for better consistency across
    plugin definitions.
    - Keep behavior unchanged while improving readability in `mason.lua`.

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,44 +1,44 @@
 -- nvim/lua/plugins/lsp/mason.lua
 
 return {
-  {
-    "mason-org/mason.nvim",
-    lazy = true,
-    build = ":MasonUpdate",
-    config = function ()
-      require('mason').setup()
-    end,
-    opts = {},
-  },
+	{
+		"mason-org/mason.nvim",
+		lazy = true,
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+		opts = {},
+	},
 
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependecies = { "mason-org/mason.nvim" },
-    cmd = {
-      "MasonToolsInstall",
-      "MasonToolsInstallSync",
-      "MasonToolsUpdate",
-      "MasonToolsUpdateSync",
-    },
-    opts = {
-      ensure_installed = {
-        -- Lua
-        -- Rust
-        "rust-analyzer",
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependecies = { "mason-org/mason.nvim" },
+		cmd = {
+			"MasonToolsInstall",
+			"MasonToolsInstallSync",
+			"MasonToolsUpdate",
+			"MasonToolsUpdateSync",
+		},
+		opts = {
+			ensure_installed = {
+				-- Lua
+				-- Rust
+				"rust-analyzer",
 
-        -- Python
-        "ruff",
-        "ty",
+				-- Python
+				"ruff",
+				"ty",
 
-        -- LaTeX
-        "texlab",
-        "latexindent",
-        -- Markdown
-        -- Shell
-        --
-      },
-      auto_update = false,
-      run_on_start = true,
-    },
-  },
+				-- LaTeX
+				"texlab",
+				"latexindent",
+				-- Markdown
+				-- Shell
+				--
+			},
+			auto_update = false,
+			run_on_start = true,
+		},
+	},
 }
