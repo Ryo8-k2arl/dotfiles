6c9ef75884906a1efffbae3ecf1500b2701f127e
Ryo @ n31's WSL <acdragon.16@mnb.ees.saitama-u.ac.jp>
2026-08-17 00:43:30 +0900

    ✨ Add Neovim command recovery for Zellij
    - Replace restored Neovim remote and socket commands with the editor
    launcher.
    - Preserve all other resurrected commands unchanged.

---
+++ .config/zellij/scripts/zellij-resurrect-command.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+command=${RESURRECT_COMMAND:-}
+
+case "$command" in
+*nvim*"--server"*"--remote-ui"* | \
+  *nvim*"--listen"*"nvim-zellij-"*)
+  printf '%s\n' \
+    "${XDG_CONFIG_HOME:-$HOME/.config}/zellij/scripts/zellij-nvim-editor.sh"
+  ;;
+
+*)
+  printf '%s\n' "$command"
+  ;;
+esac
